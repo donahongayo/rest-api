7b77426882098b2774e4c7ddc92d6edcac1df952
Donah Ongayo <donngeezzzzzzz@gmail.com>
2020-08-21 10:17:16 +0800

    refactor(model): user schema updated

--- src/models/User.ts
+++ src/models/User.ts
@@ -7,25 +7,17 @@ export interface User extends Document {
 }
 
 const schema = new Schema({
-  id: {
-    type: String,
-    index: true,
-    unique: true,
-  },
   email: {
     type: String,
-    required: true,
   },
   password: {
     type: String,
-    required: true,
   },
   name: {
     type: String,
-    required: true,
   },
 });
 
 const userSchema = new Schema(schema, { timestamps: true });
 
-export default model<User>('Users', userSchema);
+export default model<User>('users', userSchema);
