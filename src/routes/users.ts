958418d6d10c83e5993707bc63ed5a9c84e547a9
Donah Ongayo <donngeezzzzzzz@gmail.com>
2020-08-27 13:02:57 +0800

    refactor(function): paramater changes

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,15 +2,14 @@ import Router from 'koa-router';
 import User from '../models/User';
 import KoaAuth from 'koa-basic-auth';
 import mJwt from '../middleware/jwt';
+import config from '../config/config';
 import authenticateJWT from '../middleware/auth';
 
 const router = new Router();
-const username = 'hello';
-const password = 'donah';
 
 router.post(
   '/api/auth',
-  KoaAuth({ name: username, pass: password }),
+  KoaAuth({ name: config.username, pass: config.password }),
   async (ctx) => {
     ctx.body = {
       token: mJwt,
@@ -20,15 +19,19 @@ router.post(
 
 router.get('/api/users', authenticateJWT, async (ctx) => {
   await User.find()
-    .then((data) => {
-      ctx.body = data;
+    .then((users) => {
+      if (users) {
+        ctx.body = users;
+      } else {
+        ctx.body = 'No data available';
+      }
     })
     .catch((err) => {
       ctx.body = 'error ' + err;
     });
 });
 
-router.get('/api/user/:id', async (ctx) => {
+router.get('/api/user/:id', authenticateJWT, async (ctx) => {
   await User.findOne({ _id: ctx.params.id })
     .then((users) => {
       if (users) {
@@ -44,11 +47,11 @@ router.get('/api/user/:id', async (ctx) => {
 
 router.post('/api/users', async (ctx) => {
   await User.create(ctx.request.body)
-    .then((data) => {
+    .then((users) => {
       ctx.body = {
-        id: data.id,
-        email: data.email,
-        name: data.name,
+        id: users.id,
+        email: users.email,
+        name: users.name,
       };
     })
     .catch((err) => {
@@ -56,7 +59,7 @@ router.post('/api/users', async (ctx) => {
     });
 });
 
-router.patch('/api/user/:id', async (ctx) => {
+router.patch('/api/user/:id', authenticateJWT, async (ctx) => {
   const documentQuery = { _id: ctx.params.id };
   const valuesToUpdate = ctx.request.body;
   await User.updateOne(documentQuery, valuesToUpdate)
